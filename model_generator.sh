31ffa84127846c310a1508596603653d1f04c6f7
psrok1 <psrok1@gmail.com>
2016-01-24 10:21:43 -0800

    final commit, all tested

--- model_generator.sh
+++ model_generator.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-python keraswig.py create wig20_lstm LSTM wig20 --description "WIG20 LSTM complete model"
-python keraswig.py create wig20_part LSTM wig20 --description "WIG20 LSTM partial model" --limit-date 2015-11-01
-python keraswig.py create bzwbk_lstm LSTM bzw --description "BZWBK LSTM complete model"
-python keraswig.py create bzwbk_part LSTM bzw --description "BZWBK LSTM partial model" --limit-date 2015-11-01
-python keraswig.py create eur_lstm   LSTM eur --description "Eurocash LSTM complete model"
-python keraswig.py create eur_part   LSTM eur --description "Eurocash LSTM partial model" --limit-date 2015-11-01
-python keraswig.py create pkn_lstm   LSTM pkn --description "PKN Orlen LSTM complete model"
-python keraswig.py create pkn_part   LSTM pkn --description "PKN Orlen LSTM partial model" --limit-date 2015-11-01
-python keraswig.py create kgh_lstm   LSTM kgh --description "KGHM LSTM complete model"
-python keraswig.py create kgh_part   LSTM kgh --description "KGHM LSTM partial model" --limit-date 2015-11-01
-python keraswig.py create pzu_lstm   LSTM pzu --description "PZU LSTM complete model"
-python keraswig.py create pzu_part   LSTM pzu --description "PZU LSTM partial model" --limit-date 2015-11-01
-python keraswig.py create mbank_lstm LSTM mbk --description "mBank LSTM complete model"
-python keraswig.py create mbank_part LSTM mbk --description "mBank LSTM partial model" --limit-date 2015-11-01
+python keraswig.py create pkn_lstm LSTM pkn --description "PKN LSTM complete model"
+python keraswig.py create pkn_part LSTM pkn --description "PKN LSTM partial model" --limit-date 2015-11-01
+python keraswig.py create pkn_lstm3 LSTM3 pkn --description "PKN LSTM3 complete model"
+python keraswig.py create pkn_reg Regular pkn --description "PKN Regular complete model"
+python keraswig.py create pkn_gru GRU pkn --description "PKN GRU complete model"
+python keraswig.py create pkn_mlp MLP pkn --description "PKN MLP complete model"
+python keraswig.py create pkn_gru_2014 GRU pkn --description "PKN GRU partial model" --limit-date 2014-01-01
